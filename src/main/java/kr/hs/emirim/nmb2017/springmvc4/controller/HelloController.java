efe189d4a27c07b08fe353cb320f71d9f24dfdde
HyeongEun Ham <hhe000@naver.com-PC>
2017-10-10 11:07:54 +0900

    name 파라미터 처리 추가

--- src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/controller/HelloController.java
@@ -3,21 +3,18 @@ package kr.hs.emirim.nmb2017.springmvc4.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 	
 	@RequestMapping("/")
-	public String hello(Model model) {
-		model.addAttribute("message","Hello from Container!!");
+	public String hello(@RequestParam("name")String userName,Model model) {
+		model.addAttribute("message", userName+"¥‘ æ»≥Á«œººø‰!");
 		return "resultPage";
 	}
 }
 	
-//	@ResponseBody
-//	public String hello() {
-//		return "Hello, World!";
-//	}
 
 
